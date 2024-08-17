52ec4ecf407da292cbcd0a30acc71002ceca6345
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2024-08-17 23:25:24 +0530

    Added functionality to place leaves based on map

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const map = [0, 1, 0,
 
 const gridContainerElement = document.querySelector('#grid-container');
 const bombElement = "<img src=\"assets/bomb.png\" alt=\"bomb\" class=\"bomb-image\">";
+const leafElement = "<img src=\"assets/leaf.webp\" alt=\"leaf\" class=\"leaf-image\">";
 
 const getGridBoxElement = (number) => {
     return gridContainerElement.children[number-1];
@@ -17,6 +18,10 @@ const placeBombs = () => {
         if (map[i] == 1) {
             const gridBoxToPlaceBomb = getGridBoxElement(i+1);
             gridBoxToPlaceBomb.innerHTML += bombElement;
+        }
+        else if (map[i] == 0) {
+            const gridBoxToPlaceLeaf = getGridBoxElement(i+1);
+            gridBoxToPlaceLeaf.innerHTML += leafElement;
         }
     }
 }
